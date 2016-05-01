4ebd2f52e2cad6f68b2c88a819fb627eedbb572f
Dmitry Sadovnychyi <dyriax@gmail.com>
2016-05-02 01:08:13 +0800

    Add python sample

---
+++ basics/basic.py
@@ -0,0 +1,21 @@
+import BaseHTTPServer
+
+HOSTNAME = "127.0.0.1"
+PORT = 3000
+
+ARRAY = []  # Our array that will live in RAM until the server restarts
+
+
+class HttpHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+  def do_GET(self):
+    ARRAY.append("")  # appending a new entry in to our array
+    array_size = len(ARRAY)
+    self.send_response(200)
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+    return self.wfile.write(str(array_size))  # displaying the size of our array
+
+server = BaseHTTPServer.HTTPServer((HOSTNAME, PORT), HttpHandler)
+print('Server running at http://{hostname}:{port}/'.format(
+  hostname=HOSTNAME, port=PORT))
+server.serve_forever()
